0958104ecc55ffbfa5844553de67a2cd506bc465
MaxThePrisberry <bodomo9999@gmail.com>
2024-02-28 21:44:40 -0700

    Do signin javascript

---
+++ websiteFiles/signin.js
@@ -0,0 +1,51 @@
+const userInput = document.getElementById('floatingInput');
+const passInput = document.getElementById('floatingPassword');
+
+function initialize() {
+	if (localStorage.getItem('username')) {
+		document.getElementById('usernameSlot').innerText = localStorage.getItem('username');
+		document.getElementById('signinButton').innerText = "Sign Out";
+	}
+}
+
+function signinButtonClick() {
+	if (localStorage.getItem('username')) {
+		localStorage.clear();
+	}
+	window.location.href = "signin.html";
+}
+
+function login() {
+	if (!userInput.value) {
+		userInput.classList.add('is-invalid');
+	} else {
+		userInput.classList.remove('is-invalid');
+	}
+	if (!passInput.value) {
+		passInput.classList.add('is-invalid');
+	} else {
+		passInput.classList.remove('is-invalid');
+	}
+	if (!(userInput.value && passInput.value)) {console.log("SDF"); return;}
+
+	//Validate with database here
+
+	if (document.getElementById('flexCheckDefault').checked) {
+		//remembered behaviour
+		localStorage.clear();
+
+		localStorage.setItem('username', userInput.value);
+		localStorage.setItem('password', passInput.value);
+
+		location.href = "play.html";
+	} else {
+		//not remembered
+		//no cache
+		sessionStorage.clear();
+
+                sessionStorage.setItem('username', userInput.value);
+                sessionStorage.setItem('password', passInput.value);
+
+                location.href = "play.html";
+	}
+}
